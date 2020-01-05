68749929a4616afa58ace4eeca3a129eeaab1383
BTK203 <brach2003@gmail.com>
2020-01-05 16:24:20 -0600

    Added the project.

---
+++ InfiniteRecharge-2020/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,66 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  
+
+  /**
+   * Called on robot startup.
+   */
+  @Override
+  public void robotInit() {
+    
+  }
+
+  /**
+   * Called periodically while the robot is ON.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * Auto has started.
+   */
+  @Override
+  public void autonomousInit() {
+    
+  }
+
+  /**
+   * Called while auto is happening.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    
+  }
+
+  /**
+   * Called while teleop is happening
+   */
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  /**
+   * Called while in test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
