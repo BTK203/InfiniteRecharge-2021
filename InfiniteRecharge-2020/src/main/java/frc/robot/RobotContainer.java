50af3a1ca1470c54edac6fe4da74efa56d0b87fa
Marc <steve1800.j@gmail.com>
2020-01-21 10:25:42 -0600

    Updated to match BTK203 on 1/21/2020. (Github was dumb and did not let a merge happen)

---
+++ InfiniteRecharge-2020/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SubsystemClimb;
+import frc.robot.subsystems.SubsystemDrive;
+import frc.robot.subsystems.SubsystemSpinner;
+import frc.robot.subsystems.SubsystemTurret;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  /**
+   * Subsystems
+   */
+  private final SubsystemDrive   SUB_DRIVE   = new SubsystemDrive();
+  private final SubsystemTurret  SUB_TURRET  = new SubsystemTurret();
+  private final SubsystemSpinner SUB_SPINNER = new SubsystemSpinner();
+  private final SubsystemClimb   SUB_CLIMB   = new SubsystemClimb();
+
+  /**
+   * Controllers
+   */
+  private final Joystick
+    DRIVER   = new Joystick(0),
+    OPERATOR = new Joystick(1);
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+}
---
+++ InfiniteRecharge-2020/src/main/java/frc/robot/subsystems/SubsystemDrive.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SubsystemDrive extends SubsystemBase {
+  /**
+   * Creates a new SubsystemDrive.
+   */
+  public SubsystemDrive() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
