08a61cb6eead3056c217a09a9569a4542f13d1e8
BTK203 <brach2003@gmail.com>
2020-01-28 19:45:32 -0600

    Added manual turret movement, look in code for comments

--- InfiniteRecharge-2020/src/main/java/frc/robot/subsystems/SubsystemTurret.java
+++ InfiniteRecharge-2020/src/main/java/frc/robot/subsystems/SubsystemTurret.java
@@ -7,18 +7,50 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.util.Util;
+import frc.robot.util.Xbox;
 
 public class SubsystemTurret extends SubsystemBase {
   /**
    * Creates a new SubsystemTurret.
    */
+  TalonSRX 
+    turretYaw,
+    turretPitch;
+  CANSparkMax
+    turretFlywheel;
   public SubsystemTurret() {
-
+    turretYaw = new TalonSRX(Constants.TURRET_YAW_ID);
+    turretPitch = new TalonSRX(Constants.TURRET_PITCH_ID);
+    turretFlywheel = new CANSparkMax(Constants.TURRET_FLYWHEEL_ID, MotorType.kBrushless);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  // Move the turret
+  public void moveTurret(Joystick controller) {
+    double speedx;
+    double speedy;
+
+    speedx = Xbox.LEFT_X(controller);
+    speedy = Xbox.RIGHT_Y(controller);
+
+    turretYaw.set(ControlMode.PercentOutput, Util.getAndSetDouble("Turret Spin Inhibitor Yaw", speedx));
+    turretYaw.set(ControlMode.PercentOutput, Util.getAndSetDouble("Turret Spin Inhibitor Pitch", speedy));
+  }
+  // Set the flywheel speed
+  public void setFlywheelSpeed(double speedz){
+    turretFlywheel.set(speedz);
   }
 }
